85726c012140ea0eb4c6c6d636c703ca71c0d7ab
keoKAY <keokay888@gmail.com>
2023-05-11 12:07:54 +0700

    working with files in spring boot

--- src/main/java/com/istad/dataanalyticrestfulapi/controller/FileRestController.java
+++ src/main/java/com/istad/dataanalyticrestfulapi/controller/FileRestController.java
@@ -1,13 +1,16 @@
 package com.istad.dataanalyticrestfulapi.controller;
 
+import com.istad.dataanalyticrestfulapi.model.response.FileResponse;
 import com.istad.dataanalyticrestfulapi.service.FileStorageService;
 import com.istad.dataanalyticrestfulapi.utils.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/file-service")
@@ -15,10 +18,66 @@ public class FileRestController {
 
     // inject service
     @Autowired
-    FileStorageService fileStorageService ;
+    FileStorageService fileStorageService;
+
     @PostMapping("/file-upload")
-    public Response<?> fileUpload(@RequestParam MultipartFile file){
-        String filename = fileStorageService.uploadFile(file);
-        return Response.<Object>ok().setPayload(filename) ;
+    public Response<FileResponse> fileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            FileResponse response = uploadFile(file);
+            return Response.<FileResponse>ok().setPayload(response)
+                    .setMessage("Successfully upload a file ");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Failed to upload image " + ex.getMessage());
+            return Response.<FileResponse>exception().setMessage("Failed to upload an image ! Exception Occurred ! ");
+        }
     }
+
+
+    // helper method
+    @PostMapping("/multiple-file-upload")
+    public Response<List<FileResponse>> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        try {
+            List<FileResponse> responses = Arrays.stream(files)
+                    .map(this::uploadFile)
+                    .toList();
+            return Response.<List<FileResponse>>ok().setPayload(responses).setMessage("Successfully upload multiple files ");
+
+        } catch (Exception ex) {
+            System.out.println("Exception happened! " + ex.getMessage());
+            return Response.<List<FileResponse>>exception().setMessage(" Failed to upload multiple images ! Exception occurred! ");
+        }
+    }
+    // for delete single and multiple files
+
+    @DeleteMapping("/delete-file/{filename}")
+    public String deleteSingleFile(@PathVariable String filename) {
+        // method definition will be written later.
+        String result = fileStorageService.deleteFileByName(filename);
+        return result;
+    }
+
+    @DeleteMapping("/delete-all-files")
+    public String deleteAllFiles() {
+        String result = fileStorageService.deleteAllFiles();
+        return result;
+    }
+
+
+    private FileResponse uploadFile(MultipartFile file) {
+        String filename = fileStorageService.uploadFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/file-service/download-file")
+                .path(filename)
+                .toUriString();
+
+        return new FileResponse().setFilename(filename)
+                .setFileDownloadUri(fileDownloadUri)
+                .setFileType(file.getContentType())
+                .setSize(file.getSize())
+                ;
+    }
+
+
 }
--- src/main/java/com/istad/dataanalyticrestfulapi/service/serviceImpl/FileStorageServiceImpl.java
+++ src/main/java/com/istad/dataanalyticrestfulapi/service/serviceImpl/FileStorageServiceImpl.java
@@ -4,8 +4,10 @@ import com.istad.dataanalyticrestfulapi.service.FileStorageService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -45,11 +47,46 @@ public class FileStorageServiceImpl implements FileStorageService {
 
     @Override
     public String deleteFileByName(String filename) {
-        return null;
+        Path imagesLocation = Paths.get(serverLocation);
+        List<File> allFiles = List.of(imagesLocation.toFile().listFiles());
+
+        File deletedFile = allFiles.stream().filter(
+                file -> file.getName().equals(filename)
+        ).findFirst().orElse(null);
+
+        try{
+            if(deletedFile!=null){
+                Files.delete(deletedFile.toPath());
+                return  "delete file successfully ";
+            }else {
+                // cannot delete because there is no image
+                return "file with "+filename + " doesn't exit ";
+            }
+        }catch (Exception ex ){
+            System.out.println("Error delete file by name : "+ex.getMessage());
+            return  "exception occurred! failed to delete file by name ";
+        }
     }
 
     @Override
     public String deleteAllFiles() {
-        return null;
+        Path imageLocation = Paths.get(serverLocation);
+        File[] files = imageLocation.toFile().listFiles();
+        try{
+            if(files == null || files.length == 0 ){
+                return "There is no files to delete !!";
+            }
+            for(File file : files ){
+                Files.delete(file.toPath());
+            }
+            return "Successfully delete all files ";
+
+        }catch (Exception ex ){
+            ex.printStackTrace();
+            System.out.println("Exception Delete all files : "+ex.getMessage());
+            return "Failed to delete all files ! Exception occurred ! ";
+        }
+
+
     }
 }
